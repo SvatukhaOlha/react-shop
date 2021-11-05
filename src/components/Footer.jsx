209f126b5c003f49ed06929ede6a2390e7c70694
Olha Svatukha <olha.s2018@gmail.com>
2021-11-05 18:52:05 +0200

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className='page-footer green lighten-4'>
-            <div className="footer-copyright">
-                <div className="container">
-                    {new Date().getFullYear()} Copyright Text
-                    <a href="#" className="grey-text text-lighten-4 right">Repo</a>
-                </div>   
-            </div>
-        </footer>
-    )
+      <footer className='page-footer green lighten-4'>
+        <div className='footer-copyright'>
+          <div className='container'>
+            {new Date().getFullYear()} Copyright Text
+            <a
+              href='https://github.com/SvatukhaOlha/react-shop'
+              className='grey-text text-lighten-4 right'
+              target='_blank'
+            >
+              Repo
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
 }
 
 export default Footer
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 
 function Header() {
     return (
-        <nav className="green darken-1">
-            <div className="nav-wrapper">
-                <a href="#" className="brand-logo">React Shop</a>
-                <ul id="nav-mobile" className='right hide-on-med-and-down'>
-                    <li>
-                        <a href="#">Repo</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    )
+      <nav className='green darken-1'>
+        <div className='nav-wrapper'>
+          <a href='#' className='brand-logo'>
+            React Shop
+          </a>
+          <ul id='nav-mobile' className='right hide-on-med-and-down'>
+            <li>
+              <a href='https://github.com/SvatukhaOlha/react-shop' target="_blank" rel='noreferrer'>Repo</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
 }
 
 export default Header
